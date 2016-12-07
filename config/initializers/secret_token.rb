1b6b07f55ddcbd44dd5fab6bbe7be4cf0cd1953c
tenart <telnowtema@mail.ru>
2016-12-07 11:04:44 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IceMail::Application.config.secret_key_base = '459ab57227751a4c773d7d559aa596f991b8b4a2de80bc5a8abca1be5c448862f8cc5c247545036d01b5489adb1d48aa713d97ce88cf4a060c5c8125c44a343d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IceMail::Application.config.session_store :cookie_store, key: '_IceMail_session'
