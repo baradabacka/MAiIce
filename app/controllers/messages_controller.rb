3ca8822806153659d3ff895abf2393575453fc0f
tenart <telnowtema@mail.ru>
2016-12-15 13:55:40 +0300

    corrected_sorting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,13 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy, :status ]
   before_filter :authenticate_user!
-
+  before_filter :collect_states, only: [:index, :sorting]
+  before_filter :group_state, only: [:index, :sorting]
   # GET /messages
   # GET /messages.json
+
   def index
     @messages = Message.for_user(current_user)
-    @states = Message.aasm.states.map(&:name)
-    @group_state = @messages.group(:aasm_state).count
   end
 
   def status
@@ -20,15 +20,15 @@ class MessagesController < ApplicationController
   end
 
   def sorting
-    for_user = Message.for_user(current_user)
+    user_messages = Message.for_user(current_user)
     if params[:sort] == 'aasm_state'
-      stating = Message.aasm.states.map(&:name)
-      @messages = for_user.for_state(stating[0])|for_user.for_state(stating[1])|for_user.for_state(stating[2])|for_user.for_state(stating[3])
+      @messages = []
+      @states.each do |status|
+        @messages += user_messages.for_state(status)
+      end
     else
-      @messages = for_user.order(params[:sort])
+      @messages = user_messages.order(params[:sort])
     end
-    @states = Message.aasm.states.map(&:name)
-    @group_state = for_user.group(:aasm_state).count
     render 'index'
   end
 
@@ -94,6 +94,14 @@ class MessagesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
+    end
+
+    def collect_states
+      @states = Message.aasm.states.map(&:name)
+    end
+
+    def group_state
+      @group_state = Message.for_user(current_user).group(:aasm_state).count
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
